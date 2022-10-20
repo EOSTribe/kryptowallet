524cb0c81917d425d154c2ec45d754ca0a59ab33
streaming-dev <streaming0214@gmail.com>
2022-10-20 08:58:33 +0200

    Update switch action for swap

--- app/components/KIconButton.js
+++ app/components/KIconButton.js
@@ -2,13 +2,9 @@ import * as React from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const KIconButton = ({ onChange, style }) => (
-  <TouchableOpacity style={style} onPress={onChange}>
-    <MaterialIcon
-      name="swap-vertical-circle-outline"
-      style={styles.iconStyle}
-      size={30}
-    />
+const KIconButton = ({ onPress, style, name, size }) => (
+  <TouchableOpacity style={style} onPress={onPress}>
+    <MaterialIcon name={name} style={styles.iconStyle} size={size} />
   </TouchableOpacity>
 );
 
